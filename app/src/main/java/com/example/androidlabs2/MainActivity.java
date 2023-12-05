14f255314db05759579a0bd7d203e609741385d5
Alycia <alycia.curry01@gmail.com>
2023-12-05 09:15:26 -0500

    commit

--- app/src/main/java/com/example/androidlabs2/MainActivity.java
+++ app/src/main/java/com/example/androidlabs2/MainActivity.java
@@ -1,4 +1,6 @@
 package com.example.androidlabs2;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,49 +14,60 @@ import android.widget.Switch;
 import android.widget.TextView;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import java.util.ArrayList;
 import java.util.List;
+
+
 public class MainActivity extends AppCompatActivity {
     private List<Todo> todoItemList;
     private MyListAdapter todoAdapter;
+    private Opener dbOpener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        todoItemList = new ArrayList<>();
-        todoAdapter = new MyListAdapter();
+        dbOpener = new Opener(this);
+
+        SQLiteDatabase db = dbOpener.getReadableDatabase();
+        Cursor cursor = db.query(Opener.Table_Name, null, null, null, null, null, null);
+        dbOpener.printCursor(cursor);
+
+        todoItemList = dbOpener.loadData();
+        todoAdapter = new MyListAdapter(todoItemList);
 
         ListView todoList = findViewById(R.id.list_view);
         todoList.setAdapter(todoAdapter);
+
+        dbOpener.loadData();
+        todoAdapter.notifyDataSetChanged();
 
         EditText editText = findViewById(R.id.type_here_edit);
         Switch urgentSwitch = findViewById(R.id.urgent_switch);
         Button addButton = findViewById(R.id.add_button);
 
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String itemText = editText.getText().toString();
-                boolean isUrgent = urgentSwitch.isChecked();
+        addButton.setOnClickListener(v -> {
+            String itemText = editText.getText().toString();
+            boolean isUrgent = urgentSwitch.isChecked();
 
-                Todo newItem = new Todo(itemText, isUrgent);
-                todoItemList.add(newItem);
+            Todo todoItem = new Todo(itemText, isUrgent, 0);
+            todoItemList.add(todoItem);
 
-                todoAdapter.notifyDataSetChanged();
+            dbOpener.addToDB(itemText, isUrgent);
 
-                editText.setText("");
-            }
+            todoAdapter.notifyDataSetChanged();
+            editText.setText("");
         });
 
         todoList.setOnItemLongClickListener((parent, view, position, id) -> {
             AlertDialog.Builder alert = new AlertDialog.Builder(this);
             alert.setTitle("Do you want to delete this?")
-
-                    .setMessage("The selected row is:" + position)
-
+                    .setMessage("The selected row is:" + (position + 1))
                     .setPositiveButton("Yes", (click, arg) -> {
+                        int itemId = todoItemList.get(position).getId();
+
+                        dbOpener.deleteFromDB(itemId);
+
                         todoItemList.remove(position);
                         todoAdapter.notifyDataSetChanged();
                     })
@@ -64,45 +77,48 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
     }
-        private class MyListAdapter extends BaseAdapter {
-            public MyListAdapter() {
-                super();
+
+    class MyListAdapter extends BaseAdapter {
+        private List<Todo> todoItemList;
+
+        public MyListAdapter(List<Todo> todoItemList) {
+            super();
+            this.todoItemList = todoItemList;
+        }
+        public int getCount() {
+            return todoItemList.size();
+        }
+
+        public Object getItem(int position) {
+            return todoItemList.get(position);
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View old, ViewGroup parent) {
+            View newView = old;
+            LayoutInflater inflater = getLayoutInflater();
+
+            if (newView == null) {
+                newView = inflater.inflate(R.layout.activity_todo, parent, false);
             }
-            public int getCount() {
-                return todoItemList.size();
+            TextView tView = newView.findViewById(R.id.list_content);
+
+            Todo todoItem = (Todo) getItem(position);
+
+            tView.setText(todoItem.getItemText());
+
+            if (todoItem.isUrgent()) {
+                newView.setBackgroundColor(Color.RED);
+                tView.setTextColor(Color.WHITE);
+            } else {
+                newView.setBackgroundColor(Color.TRANSPARENT);
+                tView.setTextColor(Color.BLACK);
             }
 
-            public Object getItem(int position) {
-                return todoItemList.get(position);
-            }
-
-            public long getItemId(int position) {
-                return (long) position;
-            }
-
-            public View getView(int position, View old, ViewGroup parent) {
-                View newView = old;
-                LayoutInflater inflater = getLayoutInflater();
-
-                if (newView == null) {
-                    newView = inflater.inflate(R.layout.activity_todo, parent, false);
-                }
-                TextView tView = newView.findViewById(R.id.list_content);
-
-                Todo todoItem = (Todo) getItem(position);
-
-                tView.setText(todoItem.getItemText());
-
-                if (todoItem.isUrgent()) {
-                    newView.setBackgroundColor(Color.RED);
-                    tView.setTextColor(Color.WHITE);
-                } else {
-                    newView.setBackgroundColor(Color.TRANSPARENT);
-                    tView.setTextColor(Color.BLACK);
-                }
-
-                return newView;
-
+            return newView;
         }
     }
 }
--- app/src/main/java/com/example/androidlabs2/Todo.java
+++ app/src/main/java/com/example/androidlabs2/Todo.java
@@ -4,18 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Todo extends AppCompatActivity {
         private String itemText;
-        private boolean urgent;
+        private boolean Urgent;
+         private int Item_Id;
 
-        public Todo(String itemText, boolean urgent) {
-            this.itemText = itemText;
-            this.urgent = urgent;
-        }
+    public Todo(String itemText, boolean urgent, int Item_Id) {
+        this.itemText = itemText;
+        this.Urgent = urgent;
+        this.Item_Id = Item_Id;
+    }
+
 
         public String getItemText() {
             return itemText;
         }
 
         public boolean isUrgent() {
-            return urgent;
+            return Urgent;
         }
+            public int getId() {
+                return Item_Id;
+            }
     }
